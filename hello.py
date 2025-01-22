c38cad2fb0f40c5d13efc2242815e304fc36b96b
Tek Raj Bhatt <tekbhatt1@gmail.com>
2025-01-22 23:31:44 +0545

    First commit

---
+++ hello.py
@@ -0,0 +1,11 @@
+print("Hello World!")
+
+def test(param):
+    print(param)
+    
+test("Hello from test function!")
+
+# test_one = "First Test"
+# test_two = "Second Test"
+# test_three = "Third Test"
+# test_four = "Fourth test"
---
+++ hello_two.py
@@ -0,0 +1,3 @@
+from hello import test
+
+test("hello from second file!")
